c5d5a889e647646d43e21fbe55112196465f857e
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2022-02-14 19:41:16 -0500

    Tittle animation added, main page v1.0 complete

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,14 +3,17 @@ import styles from '../styles/Home.module.css'
 
 export default function Footer(): JSX.Element{
     return(
-        <footer>
-            <nav className={styles.header_nav}>
-                <ul>                    
-                    <li><Link href="blog">Blog</Link></li>
-                    <li><Link href="projects">Projects</Link></li> 
-                    <li><Link href="/">Home</Link></li>                                                           
-                </ul>                
-            </nav>
+        <footer>            
+            <div className={styles.footerContainer}>
+                <hr />
+                <nav className={styles.footerNav}>
+                    <ul>                    
+                        <li><Link href="blog">Blog</Link></li>
+                        <li><Link href="projects">Projects</Link></li> 
+                        <li><Link href="/">Home</Link></li>                                                           
+                    </ul>                
+                </nav>
+            </div>            
         </footer>
     )
 }
